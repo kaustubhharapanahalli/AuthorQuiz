12e9bb034991006a9211d74b5ae979d78c4fb234
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-10-17 15:01:30 +0530

    Add more unit test cases for AuthorQuiz.js

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,10 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AuthorQuiz from "./AuthorQuiz";
+import Enzyme, { mount, shallow, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+  turnData: {
+    books: ["The Shining", "IT", "David Copperfield", "A Tale of Two Cities"],
+    author: {
+      name: "Mark Twain",
+      imageUrl: "images/authors/marktwain.jpg",
+      imageSource: "Wikimedia Commons",
+      books: ["The Adventures of Huckleberry Finn"],
+    },
+    highlight: "none",
+  },
+};
 
 describe("Author Quiz", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<AuthorQuiz />, div);
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div);
+  });
+
+  describe("When no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}} />);
+    });
+
+    it("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe(
+        undefined
+      );
+    });
+  });
+
+  describe("When wrong answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(
+        <AuthorQuiz
+          {...Object.assign({}, state, { highlight: "wrong" })}
+          onAnswerSelected={() => {}}
+        />
+      );
+    });
+
+    it("should have a red background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe(
+        "red"
+      );
+    });
+  });
+
+  describe("When correct answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(
+        <AuthorQuiz
+          {...Object.assign({}, state, { highlight: "correct" })}
+          onAnswerSelected={() => {}}
+        />
+      );
+    });
+
+    it("should have a green background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe(
+        "green"
+      );
+    });
   });
 });
