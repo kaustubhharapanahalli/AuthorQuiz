793cc2224fddd028b080832ed04c0364761ab611
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-10-17 12:08:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AuthorQuiz from "./AuthorQuiz";
+import * as serviceWorker from "./serviceWorker";
+import { sample, shuffle } from "underscore";
+
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"],
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"],
+  },
+  {
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Daniel Ogren",
+    books: ["Harry Potter and the Sorcerers Stone"],
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Pinguino",
+    books: ["The Shining", "IT"],
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two City"],
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"],
+  },
+];
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) => title === answer)
+    ),
+  };
+}
+
+const state = {
+  turnData: getTurnData(authors),
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthorQuiz {...state} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
