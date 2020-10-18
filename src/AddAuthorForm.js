9153f5731e85b6611997676a04963237a90692de
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-10-18 18:08:02 +0530

    Add form component for Adding new author details

---
+++ src/AddAuthorForm.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import "./AddAuthorForm.css";
+
+class AuthorForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      imageUrl: "",
+      bookTemp: "",
+      books: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddBook = this.handleAddBook.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onAddAuthor({
+      name: this.state.name,
+      imageUrl: this.state.imageUrl,
+      books: this.state.books,
+    });
+    this.setState({
+      name: "",
+      imageUrl: "",
+      bookTemp: "",
+      books: [],
+    });
+  }
+
+  handleAddBook(event) {
+    this.setState({
+      books: [...this.state.books, this.state.bookTemp],
+      bookTemp: "",
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="AddAuthorForm_input">
+          <label htmlFor="name">
+            <strong>Name:</strong>
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="AddAuthorForm_input">
+          <label htmlFor="imageUrl">
+            <strong>Image URL:</strong>
+          </label>
+          <input
+            type="text"
+            name="imageUrl"
+            value={this.state.imageUrl}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="AddAuthorForm_input">
+          <label htmlFor="bookTemp">
+            <strong>Book Name:</strong>
+          </label>
+          {this.state.books.map((book) => (
+            <p key={book}>{book}</p>
+          ))}
+          <input
+            type="text"
+            name="bookTemp"
+            value={this.state.bookTemp}
+            onChange={this.handleChange}
+          />
+          <input type="button" value="+" onClick={this.handleAddBook} />
+        </div>
+        <div>
+          <input type="submit" value="Add" />
+        </div>
+      </form>
+    );
+  }
+}
+
+function AddAuthorForm({ match, onAddAuthor }) {
+  return (
+    <div className="container AddAuthorForm">
+      <h1>Add Author</h1>
+      <AuthorForm onAddAuthor={onAddAuthor} />
+    </div>
+  );
+}
+
+export default AddAuthorForm;
